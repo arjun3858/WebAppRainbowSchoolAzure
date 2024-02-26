547cdd91636fa1700c343fee164b6261122960bd
abc <abc.@gmail.com>
2024-02-26 22:33:49 +0530

    Add project files.

---
+++ WebAppRainbowSchoolAzure/Data/SchoolDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppRainbowSchoolAzure.Models;
+
+namespace WebAppRainbowSchoolAzure.Data
+{
+    public class SchoolDBContext : DbContext
+    {
+        public SchoolDBContext (DbContextOptions<SchoolDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppRainbowSchoolAzure.Models.Students> Students { get; set; } = default!;
+    }
+}
---
+++ WebAppRainbowSchoolAzure/Models/Students.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppRainbowSchoolAzure.Models
+{
+    [Table("Students")]
+    public class Students
+    {
+         
+        [Key]
+        public int StudentId { get; set; }
+        public string ?FirstName { get; set; }
+        public string ?LastName { get; set; }
+        public string ?Course { get; set; }
+        public string ?Gender { get; set; }
+
+
+    }
+}
+
